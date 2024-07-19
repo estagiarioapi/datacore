e358403b058e59d6daeee61629e587b59274e031
nsx07 <felipeaugustodluz@hotmail.com>
2024-07-19 15:38:40 -0300

    fixing up standardizeBRPhone

--- src/crosscuting/util/phone.ts
+++ src/crosscuting/util/phone.ts
@@ -1,11 +1,22 @@
 export function standardizeBRPhone(phone: string): string {
-  let standard =
-    phone.length === 11 || phone.length == 10 ? `55${phone}` : phone;
+  // Remover todos os caracteres que não sejam números
+  phone = phone.replace(/\D/g, '');
 
-  if (standard.length === 12 && standard.startsWith('55')) {
-    const split = standard.split(standard.at(4));
-    standard = split[0].concat('99') + split.slice(1).join('');
+  // Adicionar DDI se o número tiver 10 ou 11 dígitos
+  if (phone.length === 11 || phone.length === 10) {
+    phone = `55${phone}`;
   }
 
-  return standard;
+  // Corrigir números de 12 dígitos começando com 55
+  if (phone.length === 12 && phone.startsWith('55')) {
+    const fourthChar = phone.charAt(4);
+    phone = phone.replace(fourthChar, '9');
+  }
+
+  // Certificar-se de que o número final tenha 13 dígitos
+  if (phone.length !== 13) {
+    throw new Error('Número de telefone inválido');
+  }
+
+  return phone;
 }
